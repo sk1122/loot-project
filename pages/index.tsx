04818a43d57165024b531a8e5573c0156910ddc6
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-03-21 14:16:47 +0530

    Finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,20 @@ import Head from 'next/head'
 import { Disclosure } from '@headlessui/react'
 import { 
   CogIcon,
-  PlusIcon,
+  GlobeIcon,
   ChevronUpIcon
-} from '@heroicons/react/solid'
+} from '@heroicons/react/outline'
 import Web3Modal from 'web3modal'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import Authereum from "authereum";
 import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
+
+const BlockChainIcon = () => {
+  return (
+    <svg className='w-20 h-20 text-white' fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 109.06 122.88"><path d="M34.43 47.86L52.8 37.6V18.31a9.233 9.233 0 01-5.46-3.16L17.91 32.18c.35.98.54 2.03.54 3.13 0 .92-.13 1.8-.38 2.64l16.36 9.91zm11.35-35.38a9.231 9.231 0 01-.59-3.25c0-2.55 1.03-4.86 2.7-6.53S51.87 0 54.42 0c2.55 0 4.86 1.03 6.53 2.7a9.205 9.205 0 012.7 6.53c0 1.12-.2 2.19-.56 3.18l29.57 17.1c.21-.25.42-.5.65-.73a9.205 9.205 0 016.53-2.7c2.55 0 4.86 1.03 6.53 2.7a9.205 9.205 0 012.7 6.53c0 2.55-1.03 4.85-2.7 6.52a9.194 9.194 0 01-5.32 2.62v33.91c2.07.27 3.92 1.22 5.32 2.62 1.67 1.67 2.7 3.98 2.7 6.52a9.222 9.222 0 01-9.23 9.23 9.205 9.205 0 01-7.15-3.39l-29.61 17.12c.36.99.56 2.06.56 3.18 0 2.55-1.03 4.86-2.7 6.53a9.205 9.205 0 01-6.53 2.7c-2.55 0-4.86-1.03-6.53-2.7s-2.7-3.98-2.7-6.53c0-1.14.21-2.24.59-3.25L16.35 93.38a9.205 9.205 0 01-7.13 3.36c-2.55 0-4.86-1.03-6.53-2.7C1.03 92.37 0 90.06 0 87.51s1.03-4.85 2.7-6.52a9.242 9.242 0 015.25-2.62V44.44a9.18 9.18 0 01-5.25-2.62A9.164 9.164 0 010 35.3c0-2.55 1.03-4.86 2.7-6.53a9.205 9.205 0 016.53-2.7 9.205 9.205 0 017.16 3.4l29.39-16.99zm15.76 2.61a9.192 9.192 0 01-5.55 3.23V37.6l18.33 10.62 16.85-9.74c-.37-.99-.56-2.07-.56-3.18 0-1.08.19-2.13.53-3.09l-29.6-17.12zm36.69 29.3a9.159 9.159 0 01-4.92-2.56c-.19-.19-.37-.38-.54-.59l-16.82 9.72v20.78l16.89 9.75c.15-.17.3-.34.46-.5a9.194 9.194 0 014.92-2.56V44.39h.01zm-7.07 46.27c-.36-.98-.55-2.04-.55-3.14 0-1.16.21-2.27.61-3.3l-16.34-9.43-18.89 10.98v18.79a9.192 9.192 0 015.55 3.23l29.62-17.13zm-43.82 17.06a9.233 9.233 0 015.46-3.16V85.68l-18.96-11-16.09 9.29c.45 1.09.71 2.29.71 3.55 0 1.12-.2 2.19-.56 3.18l29.44 17.02zM10.76 78.41c1.93.32 3.66 1.25 4.99 2.58.1.1.19.2.28.3l16.39-9.46V50.36L16.64 40.8c-.27.37-.57.71-.89 1.03a9.255 9.255 0 01-4.99 2.58v34zM9.24 41.34c.04 0 .08-.01.12-.01h.08a6 6 0 004.06-1.76 6.023 6.023 0 001.77-4.27c0-1.67-.68-3.18-1.77-4.27-1.09-1.09-2.6-1.77-4.27-1.77s-3.18.68-4.27 1.77a6.023 6.023 0 00-1.77 4.27c0 1.67.68 3.18 1.77 4.27a6.03 6.03 0 004.28 1.77zm49.44 68.05a6.023 6.023 0 00-4.27-1.77c-1.67 0-3.18.68-4.27 1.77-1.09 1.09-1.77 2.6-1.77 4.27s.68 3.18 1.77 4.27 2.6 1.77 4.27 1.77c1.67 0 3.18-.68 4.27-1.77 1.09-1.09 1.77-2.6 1.77-4.27s-.67-3.18-1.77-4.27zm0-104.43a6.023 6.023 0 00-4.27-1.77c-1.67 0-3.18.68-4.27 1.77s-1.77 2.6-1.77 4.27c0 1.67.68 3.18 1.77 4.27a6.023 6.023 0 004.27 1.77c1.67 0 3.18-.68 4.27-1.77a6.023 6.023 0 001.77-4.27c0-1.67-.67-3.18-1.77-4.27zm45.42 78.29a6.023 6.023 0 00-4.27-1.77c-1.67 0-3.18.68-4.27 1.77a6.023 6.023 0 00-1.77 4.27c0 1.67.68 3.18 1.77 4.27a6.023 6.023 0 004.27 1.77c1.67 0 3.18-.68 4.27-1.77a6.023 6.023 0 001.77-4.27c0-1.67-.67-3.18-1.77-4.27zm-90.6 0c-1.09-1.09-2.6-1.77-4.27-1.77s-3.18.68-4.27 1.77a6.023 6.023 0 00-1.77 4.27c0 1.67.68 3.18 1.77 4.27s2.6 1.77 4.27 1.77 3.18-.68 4.27-1.77a6.023 6.023 0 001.77-4.27 6.065 6.065 0 00-1.77-4.27zm80.95-45.22c.08.08.14.18.2.28.06.1.1.2.14.31.23.34.49.66.77.95a6.023 6.023 0 004.27 1.77c1.67 0 3.18-.68 4.27-1.77a6.023 6.023 0 001.77-4.27c0-1.67-.68-3.18-1.77-4.27a6.023 6.023 0 00-4.27-1.77c-1.67 0-3.18.68-4.27 1.77a6.023 6.023 0 00-1.77 4.27c.01.99.25 1.91.66 2.73zM35.41 71.49a1.687 1.687 0 01.43.88l17.13 10.07V62.56L35.41 52.11v19.38zm37.56-19.11L55.96 62.57v19.89l17.01-10.05V52.38zM54.39 39.99l-16.6 9.93 16.69 10.05 16.21-9.84-16.3-10.14z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+  )
+}
 
 const INFURA_ID = '460f40a260564ac4a4f4b3fffb032dad'
 
@@ -18,37 +24,17 @@ const Cards = [
   {
     icon: CogIcon,
     text: `
-    CONTRACT
-    
-    MINTING
-    
-    WALK
-    
-    THROUGH`,
+    Transportation for Loot Adventures.`,
     link: ''
   },
   {
-    icon: CogIcon,
-    text: `
-    CONTRACT
-    
-    MINTING
-    
-    WALK
-    
-    THROUGH`,
+    icon: GlobeIcon,
+    text: `Art for everyone.`,
     link: ''
   },
   {
-    icon: CogIcon,
-    text: `
-    CONTRACT
-    
-    MINTING
-    
-    WALK
-    
-    THROUGH`,
+    icon: BlockChainIcon,
+    text: `All on-chain.`,
     link: ''
   }
 ]
@@ -133,7 +119,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center text-white">
       <Head>
-        <title>Create Next App</title>
+        <title>Loot Travel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -141,8 +127,8 @@ const Home: NextPage = () => {
         <div className="w-full min-h-screen flex flex-col justify-start items-center px-16 py-16 space-y-20">
           <div className="w-full h-1/3 flex justify-between items-center">
             <div className="flex flex-col justify-center items-center text-center">
-              <h1 className='text-5xl'>Banners</h1>
-              <h3 className='text-xl'>(for Adventures)</h3>
+              <h1 className='text-5xl'>Loot Travel</h1>
+              <h3 className='text-xl'>(for LootVerse)</h3>
             </div>
             {!address && 
               <button onClick={() => connectETH()} className='flex flex-col justify-center items-center text-center px-8 py-5 bg-gray-500 rounded-full border-2 border-white'>
@@ -162,14 +148,14 @@ const Home: NextPage = () => {
             </div>
             <div className="w-1/2 flex justify-center items-center space-x-5">
               {Cards.map(card => {
-                return <a href={card.link} className="w-36 h-64 bg-gray-400 flex justify-center items-center flex-col space-y-2">
+                return <a href={card.link} className="w-36 h-64 bg-gray-400 flex justify-start py-10 items-center flex-col space-y-2">
                   <card.icon className='w-20 h-20' />
                   <h1 className='text-xl font-bold'>{card.text}</h1>
                 </a>
               })}
             </div>
             <div className='border-t-2 border-t-orange-300'>
-              <h1 className='text-xl'>25000 randomized text only NFTs. Society for Loot. Art for everyone. All on-chain.</h1>
+              <h1 className='text-xl'>9999 randomized text only NFTs. Society for Loot. Art for everyone. All on-chain.</h1>
             </div>
             <div className="w-1/2 flex justify-between items-center pt-10">
               <Twitter />
@@ -185,7 +171,60 @@ const Home: NextPage = () => {
         <Disclosure>
           {({ open }) => (
             <>
+              <Disclosure.Button className="flex p-5 bg-gray-500 items-center border-b-2 border-b-white justify-between w-full px-4 py-2 mb-2 text-sm font-medium text-left">
+                <span className='text-3xl'>ROADMAP</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-white`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="text-left px-4 pt-4 pb-2 text-xl text-white">
+                Loot Travel aims to closely work with the Loot community at large to
+                develop collaborations and integrations with other projects.
+                Loot travel NFTs can be owned exclusively by Loot owners (of sLoot,
+                mLoot, and other Loot community projects).
+
+                We’re also active in the Divine DAO working on canon, lore and
+                history of the Lootverse.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure>
+          {({ open }) => (
+            <>
               <Disclosure.Button className="flex p-5 bg-gray-500 items-center border-b-2 border-b-white justify-between w-full px-4 py-2 text-sm font-medium text-left">
+                <span className='text-3xl'>ABOUT LOOT TRAVEL</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-white`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="text-left px-4 pt-4 pb-2 text-xl text-white">
+                Adventure is the spirit of Lootverse, and what’s adventure without
+                exploration! Exploration requires means of travel and this is exactly
+                what Loot Travel brings to the Lootverse.
+                In line with the vision of the original Loot Project, Loot Travel is open
+                to imagination, giving room for art and creativity to flow in – our main
+                reason to make this NFT text-only, so you can visualize it the way you
+                want and build on what we did.
+                Artists can Imagine, visualize, create and manifest the Loot Travel
+                whichever way they want!
+                Lootverse is a world where imagination flies wild and adventure rides
+                high – and Loot Travel is no less! All the modes of travel a.k.a we
+                have created are inspired by mythology, movies and folklore –
+                opening endless possibilities for exploration and storytelling in the
+                realms of the Lootverse.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex bg-gray-500 items-center border-b-2 border-b-white p-5 justify-between w-full px-4 py-2 text-sm font-medium text-left">
                 <span className='text-3xl'>ELEMENTS</span>
                 <ChevronUpIcon
                   className={`${
@@ -193,9 +232,11 @@ const Home: NextPage = () => {
                   } w-5 h-5 text-white`}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-xl text-white">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+              <Disclosure.Panel className="text-left px-4 pt-4 pb-2 text-xl text-white">
+                Everything is possible in Loot Travel - From space to flight to
+                terrestrial to water to underground movement.
+                Each NFT consists of the following components:
+                <img src="/nft.svg" className='w-72' alt="NFT" />
               </Disclosure.Panel>
             </>
           )}
@@ -211,9 +252,89 @@ const Home: NextPage = () => {
                   } w-5 h-5 text-white`}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-xl text-white">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+              <Disclosure.Panel className="px-4 text-left pt-4 pb-2 text-xl text-white">
+                <b className='text-3xl'>Vehicle #</b>
+                <br />
+                A unique serial number for each NFT.
+                Vehicles (Text in bracket is for internal understanding only)
+                <div className='px-10'>
+                  <br />
+                  <ol style={{ listStyleType: 'number' }}>
+                    <li>Millennium Falcon (space travel)</li>
+                    <li>Sling Ring (dimensional gateway)</li>
+                    <li>Nimbus 2000 Broomstick (Fly)</li>
+                    <li>Pegasus (Flying Horse)</li>
+                    <li>Flying Carpet (Fly)</li>
+                    <li>Swan Boat (water way)</li>
+                    <li>2-headed Sea Snake (underwater)</li>
+                    <li>Sail Fish (underwater)</li>
+                    <li>Four-horse chariot (Ground)</li>
+                    <li>Cheetah (Ground)</li>
+                    <li>Elephant (Ground)</li>
+                    <li>Giant Ant (underground)</li>
+                  </ol>
+                </div>
+                <br />
+                
+                <b className='text-3xl'>Rarity</b>
+                <ol className='px-10' style={{ listStyleType: 'number' }}>
+                  <br />
+                  <li>Common</li>
+                  <li>Epic</li>
+                  <li>Legendary</li>
+                  <li>Mythic</li>
+                </ol>
+                <br />
+
+                <b className='text-3xl'>Speed</b>
+                <ol className='px-10' style={{ listStyleType: 'number' }}>
+                  <br />
+                  <li>Blink of an eye</li>
+                  <li>Super Fast</li>
+                  <li>Fast</li>
+                  <li>Medium</li>
+                  <li>Slow</li>
+                  <li>Slug</li>
+                </ol>
+                <br />
+                
+                <b className='text-3xl'>Special Powers</b>
+                <ol className='px-10' style={{ listStyleType: 'number' }}>
+                  <br />
+                  <li>Spit-fire</li>
+                  <li>Shape-shifting</li>
+                  <li>Indestructible</li>
+                  <li>Cannon-shootin</li>g
+                  <li>Shielding</li>
+                  <li>Disappearance</li>
+                  <li>Self-Healing</li>
+                  <li>Camouflage</li>
+                </ol>
+                <br />
+
+                <b className='text-3xl'>Secret Metal Ingredient</b>
+                <ol className='px-10' style={{ listStyleType: 'number' }}>
+                  <br />
+                  <li>Neodymium</li>
+                  <li>Cerium</li>
+                  <li>Scandium</li>
+                  <li>Europium</li>
+                  <li>Holmium</li>
+                  <li>Erbium</li>
+                  <li>Lutetium</li>
+                </ol>
+                <br />
+                
+                <b className='text-3xl'>Color</b>
+                <ol className='px-10' style={{ listStyleType: 'number' }}>
+                  <br />
+                  <li>Gold</li>
+                  <li>Silver</li>
+                  <li>Fire</li>
+                  <li>Water</li>
+                  <li>Leaf</li>
+                  <li>Cloud</li>
+                </ol>
               </Disclosure.Panel>
             </>
           )}
@@ -229,9 +350,35 @@ const Home: NextPage = () => {
                   } w-5 h-5 text-white`}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-xl text-white">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+              <Disclosure.Panel className="text-left px-4 pt-4 pb-2 text-xl text-white">
+                <li>Each NFT will be 0.015ETH.</li>
+                <li>10 mint maximum per wallet.</li>
+                <li>By launching on Polygon (L2), gas will be mere pennies worth of
+                MATIC.</li>
+                <li>There will only ever be 9999 original NFTs which will be sold on
+                our website. Further sale will happen on opensea, etc</li>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex bg-gray-500 items-center border-b-2 border-b-white p-5 justify-between w-full px-4 py-2 text-sm font-medium text-left">
+                <span className='text-3xl'>FAQs</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-white`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="text-left px-4 pt-4 pb-2 text-xl text-white">
+                <li>
+                  <b className='text-3xl'>Why are Loot travel NFTs not designed?</b>
+                  <p>Like Loot itself, we leave the canvas open for creativity. We would
+                  love for the community and artists across the globe to interpret the
+                  Lootverse and Loot vehicles as they like and create amazing art.</p>
+                </li>
               </Disclosure.Panel>
             </>
           )}
@@ -243,8 +390,8 @@ const Home: NextPage = () => {
         <div className="w-full h-full flex justify-center items-center">
           <div className="w-full flex flex-col justify-center items-center text-center space-y-8">
             <div>
-              <h1 className='text-5xl'>Banners</h1>
-              <h3 className='text-xl'>(for Adventures)</h3>
+              <h1 className='text-5xl'>Loot Travel</h1>
+              <h3 className='text-xl'>(for LootVerse)</h3>
             </div>
             <div className="w-1/2 flex justify-between items-center">
               <Twitter />
